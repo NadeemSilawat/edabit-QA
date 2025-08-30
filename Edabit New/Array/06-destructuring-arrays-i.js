9631336e1fab776f45f4444cf7bc96e5ea3ebed7
NadeemSilawat <im.nadeem.silawat@gmail.com>
2025-08-30 22:32:11 +0530

    Destructuring Array

---
+++ Edabit New/Array/06-destructuring-arrays-i.js
@@ -0,0 +1,8 @@
+const arr = [1, 2, 3, 4, 5, 6]
+
+function test(x) {
+ return x === arr[0] || x === arr[1] ? true : false 
+}
+
+console.log(test(...arr))//, true, "Variable a does not equal 1")
+console.log(test(...arr))//, true, "Variable b does not equal 2")
